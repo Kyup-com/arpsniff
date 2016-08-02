aa33a0b02191428b15c4a1375fc896472ceb25b5
Silvestar Robev <s.robev@siteground.com>
2016-08-02 14:36:13 +0300

    Updated arpsniff2.pl as of task requests.

--- arpsniff2.pl
+++ arpsniff2.pl
@@ -3,7 +3,7 @@ use Net::Pcap;
 use NetPacket::Ethernet;
 use NetPacket::ARP;
 use Net::ARP;
-use Net::Interface;
+use POSIX qw(strftime :sys_wait_h);
 use Data::Dumper;
 
 my $VERSION = "2.0";
@@ -18,25 +18,24 @@ my $default_if = `ip r l | awk '/default/ {print \$5}'`;
 my $rc_main = '/etc/perp/arpsniff/rc.main';
 my $main_service = '/etc/perp/arpsniff/rc.main-service';
 my $rc_log = '/etc/perp/arpsniff/rc.log';
+my $logfile = '/var/log/arpsniff.log';
 
 $out_dev = $ARGV[0] if ($ARGV[0]);
 $listen_dev = $ARGV[1] if ($ARGV[1]);
 
-
 $SIG{"HUP"} = \&sigHup;
-$SIG{CHLD} = 'IGNORE';
+$SIG{"CHLD"} = \&sigChld;
+#$SIG{"TERM"} = \&sigTerm;
 
-# Collecting all interfaces in array
-sub get_interfaces {
-	my @result;
-	my @all_ifs = Net::Interface->interfaces();
-	for my $if (@all_ifs){
-		push @result, $if->name;
-	}
-	return @result;
+open CLOG, '>>', $logfile or die "Unable to open logfile $logfile: $!\n";
+# make the output to LOG and to STDOUT unbuffered
+# # this has to be done after the fork and after detaching from the command terminal
+$|=1;
+select((select(CLOG), $| = 1)[0]);
+
+sub logger {
+	    print CLOG strftime('%b %d %H:%M:%S', localtime(time)) . ' Arpsniff - ' . $_[0] . "\n";
 }
-
-@interfaces = get_interfaces;
 
 sub verify_iface {
 	return 1 if ($_[0] =~ /^veth(c[0-9]+)?[0-9]+(.[0-9]+|:[0-9]+)?$/);
@@ -52,38 +51,22 @@ sub write_newfile {
 	close $tmpcont;
 }
 
-sub should_start {
-	my $ct_if = $_[0];
-	my $result = 0;
-	$result = $running_ifs{$ct_if} if($running_ifs{$ct_if});
-	return $result;
-}
-
 # Spawning child process for each container interface
 sub start_child {
     my $ct_if = $_[0];
 	my $out_if = $default_if;
+	my $is_running = 0;
+	$is_running = $running_ifs{$ct_if} if ($running_ifs{$ct_if});
+	next if ( -d "/proc/$is_running/");
 	$out_if =~ s/[\n\r]//g;
-	next if $pid = fork;
-	$running_ifs{$ct_if} = $pid;
+	$pid = fork;
 	die "fork failed: $!" unless defined $pid;
+	if ($pid > 0) {
+		$running_ifs{$ct_if} = $pid;
+		return;
+	}
 	exec("$0 $out_if $ct_if");
 	exit;
-}
-
-sub reload_childs {
-	my $ct_if = $_[0];
-	my $out_if = $default_if;
-	$out_if =~ s/[\n\r]//g;
-	$child_exist = kill 0, $running_ifs{$ct_if};
-	if (!$child_exist) {
-		next if ($pid = fork);
-		$running_ifs{$ct_if} = $pid;
-		die "fork failed: $!" unless defined $pid;
-		exec("$0 $out_if $ct_if");
-		exit;
-	}
-	
 }
 
 # Adding perp directory with required files to run each running container interface.
@@ -96,28 +79,39 @@ sub perp_add {
 }
 
 sub run_without_params {
-	my $runtime = $_[0];
-	@interfaces = get_interfaces;
+	logger("Reloading interfaces...");
+	@interfaces = `awk '/veth/ {gsub(":",""); print \$1}' /proc/net/dev`;
 	for my $vif (@interfaces) {
-		if (verify_iface($vif) && !$runtime) {
-			start_child($vif);
-		}
-		if (verify_iface($vif) && $runtime) {
-			reload_childs($vif);
-			}
+		start_child($vif) if (verify_iface($vif));
 	}
 }
 
 sub sigHup {
-	run_without_params(1);
+	logger("SIGHUP");
+	run_without_params;
 }
 
+sub sigChld {
+    while (waitpid(-1,WNOHANG)>0 ) {
+        logger("The $pid child has been killed!");
+    }
+}
 
 if (not defined($ARGV[0]) or not defined($ARGV[1])) {
 	perp_add if ( ! -d $perp_dir);
 	run_without_params;
 	while(1) {
-		sleep(1);
+		my $res = waitpid($pid, WNOHANG);
+    	sleep(1);
+ 
+		if ($res == -1) {
+			logger("Some error occurred"), $? >> 8;
+			exit();
+		}
+		if ($res) {
+			logger("Child $res ended with "), $? >> 8;
+			last;
+		}
 	}
 }
 
@@ -155,7 +149,7 @@ sub process_packet {
 	my $tpa = join '.', map { hex } ($arp->{'tpa'} =~ /([[:xdigit:]]{2})/g);
 
 	if ($spa eq $tpa) {
-		print "Source: ",$spa,"( $mac)\tDestination: ",$tpa, "(ff:ff:ff:ff:ff:ff)\n";
+		logger("Source: ",$spa,"( $mac)\tDestination: ",$tpa, "(ff:ff:ff:ff:ff:ff)\n");
 		Net::ARP::send_packet($out_dev,			# Device
                         $tpa,					# Source IP
                         $tpa,					# Destination IP
@@ -168,4 +162,3 @@ sub process_packet {
 my $mac = Net::ARP::get_mac($out_dev) if ($out_dev);
 
 arpsniff_instance($out_dev, $listen_dev) if ($out_dev && $listen_dev);
-
